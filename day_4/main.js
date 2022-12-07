6bddd1ceb58e26b40c2350a828acc13871581e4b
RNGKing <pxp.phillips@gmail.com>
2022-12-06 22:22:51 -0600

    day 4 done and dusted, still behind by 3 days

---
+++ day_4/main.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const os = require('os');
+
+
+let data = fs.readFileSync('input.txt')
+    .toString()
+    .split(os.EOL)
+    .map(row => row.split(','))
+    .map(row => row.map(item => item.split('-')))
+    .map(row => row.map(item => item.map(entry => Number(entry))))
+    .map(row => row.map(item => Array(item[1] - item[0] + 1).fill().map((_, idx) => item[0] + idx)));
+//let mappedToArrays = segmentedArrays.map( row => row[0].split('').filter(c => row[1].split('').includes(c)));
+
+let intersections = data.map(row => row[0].filter(c => row[1].includes(c)));
+//let filtered = i
+let filtered = data.map((a, idx) => [a, intersections[idx]])
+                   .filter(row => row[0][0].length === row[1].length || row[0][1].length === row[1].length);
+
+console.log(filtered.length);
+
+let intersectionsTwo = intersections
+                        .filter(row => row.length > 0);
+console.log(intersectionsTwo.length);
